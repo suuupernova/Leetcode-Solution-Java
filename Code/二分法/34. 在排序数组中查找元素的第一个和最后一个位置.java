56796cecf3b5ce3582ec2a5f67adb9b9015c3f3b
Nausicaa <39875475+suuupernova@users.noreply.github.com>
2023-05-19 14:18:16 +0900

    Update 34. 在排序数组中查找元素的第一个和最后一个位置.java

--- Code/二分法/34. 在排序数组中查找元素的第一个和最后一个位置.java
+++ Code/二分法/34. 在排序数组中查找元素的第一个和最后一个位置.java
@@ -22,3 +22,31 @@ class Solution {
         return l;
     }
 }
+
+
+//写了新的版本
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int l=search(nums,target);
+        int r=search(nums,target+1);
+        if(l==nums.length||nums[l]!=target){
+            return new int[]{-1,-1};
+        }
+        return new int[]{l,r-1};
+        
+
+    }
+    private int search(int[] nums,int target){
+        int l=-1,r=nums.length;
+        while(l+1!=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]<target){
+                l=mid;
+            }else{
+                r=mid;
+            }
+        }
+        return r;
+        
+    }
+}
